a6b1ed69ce42345f945cca501453d58559f10d1c
lethokuhle.dev <lethokuhle345mabaso@gmail.com>
2025-06-30 18:40:07 +0200

    Add initial JavaScript functions and examples for fundamentals

---
+++ starter/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; // 'interface' is a reserved keyword in JavaScript, so it cannot be used as an identifier.
+// const private = 534; // 'private' is also a reserved keyword, so it cannot be used as an identifier.
+// const if = 23; // 'if' is a reserved keyword, so it cannot be used as an identifier.
+*/
+
+
+function logger() {
+  console.log("My name is letho")
+}
+// Calling / Running / Invoking the function
+logger();
+
+
+
+function fruitProcessor(apples, Oranges) {
+  const juice = `juice with ${apples} apples and ${Oranges} oranges`;
+  return juice;
+}
+
+ const appleJuice = fruitProcessor(5, 0);
+ console.log(appleJuice);
+
+ const appleOrangeJuice = fruitProcessor(2, 4);
+ console.log(appleOrangeJuice);
+
+ const num = Number('23')
+
+
+
+
